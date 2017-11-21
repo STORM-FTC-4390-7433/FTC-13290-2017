fdbcf8c5a261f658068f2899e57965e84205c9a2
AbhiWonKenobi <32312113+AbhiWonKenobi@users.noreply.github.com>
2017-11-21 17:06:27 -0500

    Imported an autonomous program + teleop traing

---
+++ ftc_app-3.5/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrainingTeleOp.java
@@ -0,0 +1,130 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+/*
+ * Welcome to Mr. Bross's basic Training TeleOp Code! There is nothing here but drive code,
+ * but wow, it is pretty nice and simple! This is built from a sample code provided by FIRST.
+ * Have a look under
+ * FTC-Training/FtcRobotController/java/org.firstinspires.ftc.robotcontroller/external.samples/
+ * for other great examples!
+ */
+
+/*
+ * First we have to define that this is a TeleOp Opmode by using @TeleOp. The example below is
+ * easy to follow. The name and group are not actually necessary, but useful if you want to have
+ * multiple modes. If you do not specify a name, you can only have one mode of that type (one TeleOp
+ * and one Autonomous). You can include @Disabled to disable a particular OpMode. I have commented
+ * it out because I don't want to disable it. This is useful for testing while reducing human error.
+  */
+
+@TeleOp(name="Training TeleOp", group="Linear Opmode")
+//@Disabled
+public class TrainingTeleOp extends LinearOpMode {
+
+    // Declare OpMode members.
+    // Notice that because I am using two motors on each side of the robot, they much be
+    // initialized individually.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftDrive1 = null;
+    private DcMotor leftDrive2 = null;
+    private DcMotor rightDrive1 = null;
+    private DcMotor rightDrive2 = null;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        leftDrive1  = hardwareMap.get(DcMotor.class, "left_drive_1");
+        leftDrive2 = hardwareMap.get(DcMotor.class, "left_drive_2");
+        rightDrive1 = hardwareMap.get(DcMotor.class, "right_drive_1");
+        rightDrive2 = hardwareMap.get(DcMotor.class, "right_drive_2");
+
+        // Most robots need the motor(s) on one side to be reversed to drive forward.
+        // Reverse the motor that runs backwards when connected directly to the battery.
+        // This can only be determined with live testing, but is often the left side
+        // in a traditional tank drive setup. Make sure that both motors on the same side
+        // are driving in the same direction, or else there will be serious problems!
+        leftDrive1.setDirection(DcMotor.Direction.REVERSE);
+        leftDrive2.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive1.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive2.setDirection(DcMotor.Direction.FORWARD);
+
+        // Wait for the game to start (driver presses PLAY).
+        waitForStart();
+        runtime.reset();
+
+        // This is what will actually happen while the OpMode is running,
+        // after the driver presses PLAY and before the driver presses STOP.
+        while (opModeIsActive()) {
+
+            // Setup a variable for each set of wheels to save power level for telemetry.
+            double leftPower;
+            double rightPower;
+
+            // Choose to drive using either Tank Mode, or POV Mode by commenting out the other.
+
+            // POV Mode uses left stick to go forward, and right stick to turn.
+            // - This uses basic math to combine motions and is easier to drive straight.
+            // double drive = -gamepad1.left_stick_y;
+            // double turn  =  gamepad1.right_stick_x;
+            // leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
+            // rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+
+            // Tank Mode uses one stick to control each wheel.
+            // - This requires no math, but it is hard to drive forward slowly and keep straight.
+            // We have to invert the y-dimension of each stick, because +1 points backwards.
+            leftPower  = -gamepad1.left_stick_y;
+            rightPower = -gamepad1.right_stick_y;
+
+            // Send power to wheels. If any calculations or adjustments are necessary, do them first.
+            // We used to need to clip the input from -1 to +1, but that is no longer necessary.
+            leftDrive1.setPower(leftPower);
+            leftDrive2.setPower(leftPower);
+            rightDrive1.setPower(rightPower);
+            rightDrive2.setPower(rightPower);
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+            telemetry.update();
+        }
+    }
+}
